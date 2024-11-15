44fe6b655e5061174046c9a5b1500634c05ad322
ZzhenZhou <140656561+ZzhenZhou@users.noreply.github.com>
2024-11-15 20:07:26 +0000

    first commit

--- Program.cs
+++ Program.cs
@@ -1,12 +1,30 @@
 
 
 
+using Ensek.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+void ConfigureServices(IServiceCollection services)
+{
+    var exepath = Path.GetDirectoryName(System.Reflection.Assembly
+                                        .GetExecutingAssembly().Location);
+
+    var configuration = new ConfigurationBuilder()
+                            .SetBasePath(exepath)
+                            .AddJsonFile("appsettings.json").Build();
+
+    services.AddDbContext<MeterRecordContext>(options =>
+    {
+        options.UseSqlServer(configuration.GetSection("Ensek").Value);
+    });
+
+
+}
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
